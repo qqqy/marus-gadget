8317e62b3881b834643ff39481bd2edad9c92ee5
qqqy <frogasaurus@hotmail.com>
2019-08-26 15:41:12 -0600

    perDiem prototyped

---
+++ src/data/fruits.js
@@ -0,0 +1,19 @@
+export default {
+  strawberry: {
+    grow: 9,
+    regrow: 4,
+    yeild: 1,
+    seedBuy: 100,
+    trellis: false,
+    prices: [
+      {baseProduce: 120} ,
+      {silverProduce: 150} ,
+      {goldProduce: 180} ,
+      {preserve: 290} ,
+      {baseWine: 360} ,
+      {silverWine: 450} ,
+      {goldWine: 540} ,
+      {iridiumWine: 720} ,
+    ]
+  }
+}
